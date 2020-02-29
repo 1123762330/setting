dd42932e39ba40e54848c49e96a9a30ab9ec6b45
ZLY <LR1482053>
2020-02-29 17:44:49 +0800

    配置注册服务

--- src/main/java/com/xnpool/setting/SettingApplication.java
+++ src/main/java/com/xnpool/setting/SettingApplication.java
@@ -3,7 +3,13 @@ package com.xnpool.setting;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableFeignClients //Feign
+@EnableEurekaClient
 @SpringBootApplication
 @MapperScan("com.xnpool.setting.domain.mapper")
 public class SettingApplication {
