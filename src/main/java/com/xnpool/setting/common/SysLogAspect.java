7b8d4cca747dc9d7b08ea4c5ff992badf6b38b6f
ZLY <LR1482053>
2020-03-15 19:59:35 +0800

    代码整理

--- src/main/java/com/xnpool/setting/common/SysLogAspect.java
+++ src/main/java/com/xnpool/setting/common/SysLogAspect.java
@@ -1,34 +1,35 @@
-//package com.xnpool.setting.common;
-//
-//
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * 日志切面注解
-// * @author zly
-// * @version 1.0
-// * @date 2020/3/13 16:41
-// */
-//@Aspect
-//@Component
-//public class SysLogAspect {
-//    @Autowired
-//    private WriteLogUtil writeLogUtil;
-//    //定义切点 @Pointcut
-//    //在注解的位置切入代码
-//    @Pointcut("@annotation(com.xnpool.logaop.annotation.SystemLog)")
-//    public void logPoinCut() {
-//    }
-//
-//
-//    @Around("logPoinCut()")
-//    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
-//        return writeLogUtil.writeLog(proceedingJoinPoint);
-//    }
-//
-//}
+package com.xnpool.setting.common;
+
+
+import com.xnpool.logaop.util.WriteLogUtil;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 日志切面注解
+ * @author zly
+ * @version 1.0
+ * @date 2020/3/13 16:41
+ */
+@Aspect
+@Component
+public class SysLogAspect {
+    @Autowired
+    private WriteLogUtil writeLogUtil;
+    //定义切点 @Pointcut
+    //在注解的位置切入代码
+    @Pointcut("@annotation(com.xnpool.logaop.annotation.SystemLog)")
+    public void logPoinCut() {
+    }
+
+
+    @Around("logPoinCut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        return writeLogUtil.writeLog(proceedingJoinPoint);
+    }
+
+}
--- src/main/java/com/xnpool/setting/service/impl/UserWebService.java
+++ src/main/java/com/xnpool/setting/service/impl/UserWebService.java
@@ -21,74 +21,72 @@ public class UserWebService extends BaseController {
     private JedisUtil jedisUtil;
 
     /**
+     * @return
      * @Description 矿机算力曲线图
      * @Author zly
      * @Date 15:05 2020/3/13
      * @Param
-     * @return
      */
-    public Map<Object, Object>  getWorkerHashByDay(String token) {
-        int userId=1;
+    public Map<Object, Object> getWorkerHashByDay(String token) {
+        int userId = 1;
         Map<Object, Object> resultMap = new HashMap<>();
         //先生成一个96个点的数据map
         HashMap<Object, Object> middleMap = qiegeMin(15);
-        String bigKey = (HASHRATE_DATA+userId);
+        String bigKey = (HASHRATE_DATA + userId);
         Boolean result = jedisUtil.exists(bigKey);
         if (result) {
             //取出24小时数据
             Map<String, String> workerAvgHash = jedisUtil.hgetall(bigKey);
             for (Map.Entry<Object, Object> entry : middleMap.entrySet()) {
                 String value = workerAvgHash.get(entry.getKey());
-                if (!StringUtils.isEmpty(value)){
-                    resultMap.put(entry.getKey(),value);
-                }else {
-                    resultMap.put(entry.getKey(),"0");
+                if (!StringUtils.isEmpty(value)) {
+                    resultMap.put(entry.getKey(), value);
+                } else {
+                    resultMap.put(entry.getKey(), "0");
                 }
             }
+        }else {
+            resultMap=middleMap;
         }
-        if (middleMap.size() == 0) {
-            return null;
-        } else {
-            //将map转成treeMap排序,然后键和values直接转
-            Map<Object, Object> sortedMap = new TreeMap<>(resultMap);
-            return sortedMap;
-        }
+        //将map转成treeMap排序,然后键和values直接转
+        Map<Object, Object> sortedMap = new TreeMap<>(resultMap);
+        return sortedMap;
     }
 
     /**
+     * @return
      * @Description 矿机数量折线图
      * @Author zly
      * @Date 15:03 2020/3/13
      * @Param
-     * @return
      */
     public Map<Object, Object> getWorkerTotalByDay(String token) {
         //后期从token中获取用户Id
-        int userId=1;
+        int userId = 1;
         Map<Object, Object> resultMap = new HashMap<>();
         //先生成一个96个点的数据map
         HashMap<Object, Object> middleMap = qiegeMin(15);
-        String bigKey = (ON_LINE_DATA+userId);
+        String bigKey = (ON_LINE_DATA + userId);
         Boolean result = jedisUtil.exists(bigKey);
         if (result) {
             //取出24小时数据
             Map<String, String> workerAvgHash = jedisUtil.hgetall(bigKey);
             for (Map.Entry<Object, Object> entry : middleMap.entrySet()) {
                 String value = workerAvgHash.get(entry.getKey());
-                if (!StringUtils.isEmpty(value)){
-                    resultMap.put(entry.getKey(),value);
-                }else {
-                    resultMap.put(entry.getKey(),"0");
+                if (!StringUtils.isEmpty(value)) {
+                    resultMap.put(entry.getKey(), value);
+                } else {
+                    resultMap.put(entry.getKey(), "0");
                 }
             }
+        }else {
+            resultMap=middleMap;
         }
-        if (middleMap.size() == 0) {
-            return null;
-        } else {
-            //将map转成treeMap排序,然后键和values直接转
-            Map<Object, Object> sortedMap = new TreeMap<>(resultMap);
-            return sortedMap;
-        }
+
+        //将map转成treeMap排序,然后键和values直接转
+        Map<Object, Object> sortedMap = new TreeMap<>(resultMap);
+        return sortedMap;
+
     }
 }
 
